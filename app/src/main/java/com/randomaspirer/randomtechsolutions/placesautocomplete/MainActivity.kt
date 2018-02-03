16f2ce0434056d8dd55bf85773fff4887a4ea0c8
Tankasala, Nandeesh <Nandeesh.Tankasala@meijer.com>
2018-02-03 18:50:39 -0500

    Initial commit

---
+++ app/src/main/java/com/randomaspirer/randomtechsolutions/placesautocomplete/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.randomaspirer.randomtechsolutions.placesautocomplete
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
