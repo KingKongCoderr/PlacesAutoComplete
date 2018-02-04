90398a90c12289b459d01b77af5a894095a86832
Tankasala, Nandeesh <Nandeesh.Tankasala@meijer.com>
2018-02-04 03:19:42 -0500

    added default and custom autocomplete ui widgets( pending implementation of them)

--- app/src/main/java/com/randomaspirer/randomtechsolutions/placesautocomplete/MainActivity.kt
+++ app/src/main/java/com/randomaspirer/randomtechsolutions/placesautocomplete/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.randomaspirer.randomtechsolutions.placesautocomplete
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.randomaspirer.randomtechsolutions.placesautocomplete.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var 
+    private lateinit var activityMainBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
     }
+
+    fun openAutoCompleteFragment(view : View) {
+        if(view.id == R.id.defaultAutocomplete){
+            //call fragment
+        }
+    }
+
 }
